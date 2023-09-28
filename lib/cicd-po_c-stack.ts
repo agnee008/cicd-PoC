487f6f1e738b1f638ff394ed6e7456319a91339f
agnee008 <130479712+agnee008@users.noreply.github.com>
2023-09-28 16:42:04 +0100

    updates

--- lib/cicd-po_c-stack.ts
+++ lib/cicd-po_c-stack.ts
@@ -1,21 +1,33 @@
 import * as cdk from 'aws-cdk-lib';
+import * as codecommit from 'aws-cdk-lib/aws-codecommit';
 import { Construct } from 'constructs';
-import { CodePipeline, CodePipelineSource, ShellStep, Step } from 'aws-cdk-lib/pipelines';
-import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
-// import { MyPipelineAppStage } from './stage';
+import {CodeBuildStep, CodePipeline, CodePipelineSource} from "aws-cdk-lib/pipelines";
 
 export class CicdPoCStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    new CodePipeline(this, 'Pipeline', {
-      pipelineName: 'PoCPipeline',
-      synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('agnee008/cicd-PoC', 'main'),
-        commands: ['npm ci', 
-                   'npm run build', 
-                   'npx cdk synth']
-      })
-    });
-  }
+        // This creates a new CodeCommit repository called 'CICDRepo'
+        const repo = new codecommit.Repository(this, 'CICDRepo', {
+            repositoryName: "CICDRepo"
+        });
+
+        // The basic pipeline declaration. This sets the initial structure
+        // of our pipeline
+       new CodePipeline(this, 'Pipeline', {
+            pipelineName: 'PoCPipeline',
+            synth: new CodeBuildStep('SynthStep', {
+                    input: CodePipelineSource.codeCommit(repo, 'main'),
+                    installCommands: [
+                        'npm install -g aws-cdk'
+                    ],
+                    commands: [
+                        'npm ci',
+                        'npm run build',
+                        'npx cdk synth'
+                    ]
+                }
+            )
+        });
+    }
 }
